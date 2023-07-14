20f08833eda90c353020b9a0dbbe909aae0e4f3d
jaeryo2357 <jaeryo2357@naver.com>
2023-07-15 06:03:16 +0900

    WebView Render Process 잡기

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.webview.crash.RenderCrashWebViewScreen
 import com.example.myapplication.webview.youtube.YouTubeInWebViewScreen
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                YouTubeInWebViewScreen()
+                RenderCrashWebViewScreen()
             }
         }
     }
